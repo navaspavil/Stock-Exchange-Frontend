4dda269788b0d9a2b6413c4b3fa46ba2f1e934e2
Muhammed Navas <navaspavil@gmail.com>
2024-01-19 00:43:00 +0530

    Toast Component

---
+++ src/components/Toast.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    v-if="visible"
+    class="fixed top-5 right-5 border p-4 rounded-lg shadow-lg"
+    :class="{
+      'bg-red-50 text-red-600 border-red-600': isError,
+      'bg-green-50 text-green-600 border-green-600': !isError,
+    }"
+  >
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToastComponent",
+  data() {
+    return {
+      visible: false,
+      message: "",
+      isError: false,
+    };
+  },
+  methods: {
+    show(msg, error = false) {
+      this.message = msg;
+      this.isError = error;
+      this.visible = true;
+      setTimeout(() => {
+        this.visible = false;
+      }, 3000);
+    },
+  },
+};
+</script>
